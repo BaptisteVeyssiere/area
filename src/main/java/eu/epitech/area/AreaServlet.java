2f9b4f99381dd3d9000b7a31483055277269e7ae
Baptiste Veyssiere <baptiste.veyssiere@epitech.eu>
2017-10-27 16:01:52 +0200

    server clean with google authentification

--- src/main/java/eu/epitech/area/AreaServlet.java
+++ src/main/java/eu/epitech/area/AreaServlet.java
@@ -5,22 +5,22 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.PrintWriter;
 
 public class AreaServlet extends HttpServlet {
-    public void doGet(HttpServletRequest request, HttpServletResponse response ) throws ServletException, IOException {
-        response.setContentType("text/html");
-        response.setCharacterEncoding( "UTF-8" );
-        PrintWriter out = response.getWriter();
-        out.println("<!DOCTYPE html>");
-        out.println("<html>");
-        out.println("<head>");
-        out.println("<meta charset=\"utf-8\" />");
-        out.println("<title>Test</title>");
-        out.println("</head>");
-        out.println("<body>");
-        out.println("<p>Ceci est une page générée depuis une servlet.</p>");
-        out.println("</body>");
-        out.println("</html>");
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        /* Création et initialisation du message. */
+        String paramAuteur = request.getParameter( "auteur" );
+        String message = "Transmission de variables : OK ! " + paramAuteur;
+
+	/* Création du bean */
+        Coyote premierBean = new Coyote();
+	/* Initialisation de ses propriétés */
+        premierBean.setNom( "Coyote" );
+        premierBean.setPrenom( "Wile E." );
+
+	/* Stockage du message et du bean dans l'objet request */
+        request.setAttribute( "test", message );
+        request.setAttribute( "coyote", premierBean );
+        this.getServletContext().getRequestDispatcher("/WEB-INF/index.jsp").forward(request, response);
     }
 }
