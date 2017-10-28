f79fd4969fd34e8a2a83c29fad661dc5cb954711
Nathan Scutari <nathan.scutari@epitech.eu>
2017-10-28 23:45:12 +0200

    twittermodule

---
+++ src/main/java/eu/epitech/area/TwitterModule.java
@@ -0,0 +1,20 @@
+package eu.epitech.area;
+
+import twitter4j.Twitter;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class TwitterModule extends IModule {
+    private Twitter twitter = null;
+
+    public  TwitterModule() {
+        List<ITrigger> triggers = new LinkedList<ITrigger>();
+        List<IReaction>  reactions = new LinkedList<IReaction>();
+
+        setName("Twitter");
+        triggers.add(new GetUserTweets(twitter));
+        setReactions(reactions);
+        setTriggers(triggers);
+    }
+}
