b6e266447471a692adcd6c7ce42fb5c1bdfe7adf
Nathan Scutari <nathan.scutari@epitech.eu>
2017-10-29 18:06:18 +0100

    getLink

--- src/main/java/eu/epitech/area/TwitterModule.java
+++ src/main/java/eu/epitech/area/TwitterModule.java
@@ -1,12 +1,17 @@
 package eu.epitech.area;
 
 import twitter4j.Twitter;
+import twitter4j.TwitterFactory;
+import twitter4j.auth.AccessToken;
+import twitter4j.auth.RequestToken;
 
 import java.util.LinkedList;
 import java.util.List;
 
 public class TwitterModule extends IModule {
     private Twitter twitter = null;
+    private AccessToken acessToken;
+    private RequestToken requestToken;
 
     public  TwitterModule() {
         List<ITrigger> triggers = new LinkedList<ITrigger>();
@@ -19,6 +24,13 @@ public class TwitterModule extends IModule {
     }
 
     public String   getLink() {
-        return ("lol");
+        twitter = TwitterFactory.getSingleton();
+        twitter.setOAuthConsumer("ZVhu7BPEbh9TaTQDLVUVvC0lO", " dNhGF4xlQWtiCQctF16DWufOpFWF9a1YsFEx0qXkAARaB2x9kw");
+        try {
+            requestToken = twitter.getOAuthRequestToken("http://localhost:8080/module");
+        } catch (twitter4j.TwitterException e) {
+            return (null);
+        }
+        return (requestToken.getAuthenticationURL());
     }
 }
